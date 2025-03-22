6a1eff74cf43b2384c169a85d555e3877705e09a
Xinhao Xie <xxhxiexinhao@gmail.com>
2025-03-21 17:56:37 -0700

    added three buttons

--- CalendarApp/src/application/Main.java
+++ CalendarApp/src/application/Main.java
@@ -4,13 +4,15 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
